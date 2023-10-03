b95a656771319c41821405ae52235d447d3f43e8
Dwikavindra Haryo Radithya <dwikavindra.haryo.r@mail.ugm.ac.id>
2023-10-03 04:42:48 +0100

    Added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div className="flex flex-col text-center pb-10 pt-10 w-[150] mt-10 bg-[#202020]">
+      <h1 className="text-2xl">Dwikavindra</h1>
+      <h1 className="text-2xl mt-3">Circa 2023</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,19 @@ import Education from "../../components/Education/Education";
 import WorkExperience from "../../components/WorkExperience/WorkExperience";
 import Projects from "../../components/Projects/Projects";
 import Socials from "../../components/Socials/Socials";
+import Footer from "../../components/Footer";
 
 export default function Home() {
   return (
-    <div className="ml-5 mr-5 xl:ml-20">
-      <About></About>
-      <Education></Education>
-      <WorkExperience></WorkExperience>
-      <Projects></Projects>
-      <Socials></Socials>
+    <div>
+      <div className="ml-5 mr-5 xl:ml-20">
+        <About></About>
+        <Education></Education>
+        <WorkExperience></WorkExperience>
+        <Projects></Projects>
+        <Socials></Socials>
+      </div>
+      <Footer />
     </div>
   );
 }
